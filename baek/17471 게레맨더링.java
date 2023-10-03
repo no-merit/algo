1e2e173985b02b05b7f59512478bdf619680df41
no-merit <128347576+no-merit@users.noreply.github.com>
2023-10-03 20:55:02 +0900

    17471 게레맨더링.java

--- baek/17471 게레맨더링.java
+++ baek/17471 게레맨더링.java
@@ -9,7 +9,9 @@ import java.util.Queue;
 
 /*N개의 구역으로 나뉨 2~10
  *  구역의 인구수 1~100
- * 선거구는 무조건 2개 
+ * 선거구는 무조건 2개
+ * 
+ * 
  * */
 public class Main {
 	static StringBuilder sb;
@@ -28,9 +30,9 @@ public class Main {
 		// 구역의 개수
 
 		String[] zones = br.readLine().split(" ");
-		zone = new int[N];//해당 구역의 인구수 저장장
-		list = new ArrayList<List<Integer>>(); // 인접리스트 생성
-		district = new int[N]; // 어떤 구역에 속할지 저장하는 배열
+		zone = new int[N];
+		list = new ArrayList<List<Integer>>();
+		district = new int[N];
 		
 		
 		for (int i = 0; i < N; i++) {
@@ -45,7 +47,44 @@ public class Main {
 			int size = Integer.parseInt(edge[0]);
 			// i번 구역의 인접 정점
 			for (int j = 1; j <= size; j++) {
-				list.get(i).add(I수
+				list.get(i).add(Integer.parseInt(edge[j]));
+				// 0번 리스트인 1번 구역의 인접 정점은 4번과 2번
+			}
+		} // 인접 리스트 생성
+		dfs(0);
+		
+		if(Min==Integer.MAX_VALUE) {
+			sb.append(-1);
+		}else {
+			sb.append(Min);
+		}
+		
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+	static void dfs(int depth) {
+		if (depth == N) {
+			visited = new boolean[N]; 
+			
+			int cnt =0;
+			int a=0; int b=0;
+			
+			for (int i = 0; i < N; i++) {
+				if (!visited[i]) {// 선거구 1인 구역
+					bfs(i);
+					cnt++;
+				}
+			}
+			
+			if(cnt==2) {
+				for(int i=0; i<N; i++) {
+					if(district[i]==0) {
+						a+=zone[i];
+					}else {
+						b+=zone[i];
 					}
 				}
 				Min = Math.min(Min, Math.abs(a-b));
